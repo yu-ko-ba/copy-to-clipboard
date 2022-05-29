06702e48ccb069924710c4d83ea0444c539ce51a
yu-ko-ba <yu-ko-ba@outlook.com>
2022-05-29 13:38:17 +0900

    GitHub Pagesにデプロイできるようにした

---
+++ src/themes/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material"
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#bbdefb",
+    },
+    secondary: {
+      main: "#1565c0",
+      },
+  },
+});
